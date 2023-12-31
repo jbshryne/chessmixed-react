d6bdbb44575669a071c9c68c736fb6e416a7417f
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-12-30 19:32:41 -0500

    attempt to fix socket

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,8 @@ import io from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3200";
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : process.env.REACT_APP_API_URL;
 
 export const socket = io(URL);
